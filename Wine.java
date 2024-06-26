c361c71cf9a1e6fe3dfddfc4897cad54de04ab78
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-06-27 01:12:33 +0530

    Create Wine.java

---
+++ Wine.java
@@ -0,0 +1,32 @@
+package com.example.winestoreapp2;
+
+public class Wine {
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+
+    public Wine(String name, String description, double price, String imageUrl) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
+
